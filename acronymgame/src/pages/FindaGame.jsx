752e6bd93f5810b502364c14f8f5e81c5f787193
Alestir <arondeau@uwo.ca>
2024-04-06 13:25:33 -0400

    fixed formatting

--- acronymgame/src/pages/FindaGame.jsx
+++ acronymgame/src/pages/FindaGame.jsx
@@ -1,9 +1,24 @@
 //import logo from './4me.png';
 import './FindaGame.css';
-import react, {useState} from 'react';
+import React, {useState} from 'react';
 
 const FindaGame = ({location}) => {
   const [Height, setHeight] = useState('100hv');
+  const [user, setUser] =useState({
+    nameR: '',
+    nameS: '',
+    nameC: '',
+    code: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+  console.log(user.code)
   return (location.pathname === '/FindaGame')
   ? (
     <div className="App" style={{ height: Height}}>
@@ -13,40 +28,71 @@ const FindaGame = ({location}) => {
       <div className = "container">
       <h2 className = "findrandomgame"> 
         Search for a random lobby
-        <div >
+        <div className = "space">
           <div>
             Name
           </div>
           <input
+            className = "textbox"
             type="text"
-            name="name"
+            name="nameR"
+            value={user.nameR}
+            onChange={handleChange}
             />
+            
         </div>
+        <button className = "button-84">
+          Join Game
+        </button>
       </h2>
       <h2 className = "entergamecode"> 
         Search for a lobby using a code
-        <div >
+        <div className = "space">
           <div>
             Name
           </div>
           <input
+            className = "textbox"
             type="text"
-            name="name2"
+            name="nameS"
+            value={user.nameS}
+            onChange={handleChange}
             />
         </div>
-        <div >
+        <div className = "space">
           <div>
             Code
           </div>
           <input
+            className = "textbox"
             type="text"
             name="code"
+            value={user.code}
+            onChange={handleChange}
             />
         </div>
+        <button className = "button-84">
+          Join Game
+        </button>
       </h2>
       </div>
       <h3 className = "createagame">
         Create a Lobby
+        <div >
+          <div>
+            Name
+          </div>
+          <input
+            className = "textbox2"
+            type="text"
+            name="nameC"
+            value={user.nameC}
+            onChange={handleChange}
+            />
+        </div>
+        <button className = "button-2">
+          Join Game
+        </button>
       </h3>
     </div>
   ): null;
